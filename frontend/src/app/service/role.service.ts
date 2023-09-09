bf220f67859001a8c05eea1a937ece5dcdcbd276
daneker <kkraken2005@gmail.com>
2023-09-09 20:02:12 +0600

    role service ts

---
+++ frontend/src/app/service/role.service.ts
@@ -0,0 +1,37 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Role} from "../domain/Role";
+import {Observable} from "rxjs";
+import {API} from "../config/config";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+
+  private http = inject(HttpClient)
+
+  assign(
+    userId: number,
+    role: Role
+  ): Observable<any> {
+    return this.http.post(`${API}/role/assign`, {}, {
+      params: {
+        'user_id': userId,
+        'role': role
+      }
+    })
+  }
+
+  deprive(
+    userId: number,
+    role: Role
+  ): Observable<any> {
+    return this.http.post(`${API}/role/deprive`, {}, {
+      params: {
+        'user_id': userId,
+        'role': role
+      }
+    })
+  }
+}
